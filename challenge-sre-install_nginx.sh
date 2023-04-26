9af2b89d435849ab3e1e08c56362e3ca3861c111
Oswaldo Galdino <130579714+oswaldo-jr@users.noreply.github.com>
2023-04-26 09:43:16 -0300

    Update challenge-sre-install_nginx.sh

--- challenge-sre-install_nginx.sh
+++ challenge-sre-install_nginx.sh
@@ -38,7 +38,7 @@ firewall-cmd --list-all
 echo ""
 echo " Testando se o serviço NGINX está ativo"
 systemctl status nginx
-wget https://challenge-sre-2023s.s3.amazonaws.com/challenge.tar.gz
+wget https://challenge-sre-2023s.s3.amazonaws.com/challenge.zip
 chmod +x challenge.tar.gz
 tar -xzf challenge.tar.gz
 mv challenge/* /root
